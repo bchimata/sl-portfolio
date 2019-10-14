a8280a35849e3fe05fbac4870aaf6882297d03e9
Babu Chimata <bchimata@gmail.com>
2019-10-14 14:30:03 -0500

    updated with tested lambda code

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,23 +5,24 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    # TODO implement
 
     s3 = boto3.resource('s3')
-    portfolio_bucket = s3.Bucket('babu.chimata.info')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:853693707439:deploymentPortfolioTopic')
+    try:
 
-    build_bucket = s3.Bucket('potfoliobuilddev.chimata.info')
-
-    portfolio_zip = BytesIO()
-    build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
-
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj,nm,
-            ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Hello from Lambda!')
-        }
+        portfolio_bucket = s3.Bucket('babu.chimata.info')
+        build_bucket = s3.Bucket('potfoliobuilddev.chimata.info')
+        portfolio_zip = BytesIO()
+        build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj,nm,
+                ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+                topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully")
+    except:
+        topic.publish(Subject="Portfolio deployment Failed", Message="Portfolio deployment failed")
+        raise
+    return 'Hello from Lambda!'
